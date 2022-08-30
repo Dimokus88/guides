f39c0c064c6049cabaec42e8dd2140c9d43ec9e1
Dimokus <Dimokus@mail.ru>
2022-08-30 18:13:24 +0300

    Create main.sh

---
+++ Cosmos SDK/main.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# By Dimokus (https://t.me/Dimokus)
+#-----------КОМПИЛЯЦИЯ БИНАРНОГО ФАЙЛА------------
+git clone https://github.com/cosmos/gaia.git
+cd gaia && make install
+
+cp $HOME/gaia/build/gaiad /usr/bin/
+cp $HOME/go/bin/gaiad /usr/bin/
+gaiad version
+
+gaiad  init MONIKER --chain-id GAIA 
+
+cat /root/.gaia/config/priv_validator_key.json
+sleep infinity
